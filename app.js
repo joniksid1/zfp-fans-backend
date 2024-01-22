cd7dd46ff3384b18b948145c8a8b54fdbec46616
Mikhail Kuznetsov <jonik.sid1@gmail.com>
2024-01-22 21:04:11 +0300

    feat: add datasheet content with db data and plot

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const cors = require('cors');
 const { router } = require('./routes/root');
 const { NotFoundError } = require('./utils/errors/not-found-error');
 const error = require('./middlewares/error');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { getDataSheet } = require('./controllers/datasheet');
+const { requestLogger, errorLogger } = require('./middlewares/logger')
 
 const { PORT = '3000', MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
 
@@ -44,7 +43,7 @@ app.use(cors({
 
 app.use(express.raw());
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use(requestLogger);
 
--- controllers/datasheet.js
+++ controllers/datasheet.js
@@ -1,48 +1,147 @@
-const ExcelJS = require('exceljs');
-const path = require('path');
-const fs = require('fs');
+const ExcelJS = require('exceljs'); // Подключаем библиотеку для работы с Excel
+const path = require('path'); // Подключаем модуль для работы с путями
+const fs = require('fs').promises; // Используем промисифицированный fs для работы с файловой системой
+const Jimp = require('jimp'); // Подключаем библиотеку для работы с изображениями
+const NotFoundError = require('../utils/errors/not-found-error'); // Подключаем свою ошибку
 
+// Экспортируем функцию обработки запроса
 module.exports.getDataSheet = async (req, res) => {
-  const templatePath = path.join(__dirname, '../template/data-worksheet.xlsx');
+  const templatePath = path.join(__dirname, '../template/data-worksheet.xlsx'); // Формируем путь к шаблону Excel
+  const { db } = req; // Извлекаем объект базы данных из запроса
+  const selectedData = req.body.historyItem; // Извлекаем необходимые данные из запроса
 
-  // Загружаем шаблон Excel-файла
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.readFile(templatePath);
-  const worksheet = workbook.getWorksheet('Техника'); // Имя листа, надо проверить
+  // Создаем изображение из base64
+  const buffer = Buffer.from(selectedData.plotImage.split(',')[1], 'base64');
+  const jimpImage = await Jimp.read(buffer);
 
-  // Заполняем данные в ячейки
-  worksheet.getCell('E9').value = 'blabla';
+  // Получаем буфер изображения
+  const imageBuffer = await jimpImage.getBufferAsync(Jimp.MIME_PNG);
 
-  // Сохраняем результат в новый файл Excel
-  const outputPath = path.join(__dirname, '../downloads/newDataSheet.xlsx');
+  // Сохраняем изображение
+  const imagePath = './uploads/image.png';
+  await jimpImage.writeAsync(imagePath);
 
-  workbook.xlsx.writeFile(outputPath)
-    .then(() => {
-      console.log('File is written');
+  let outputPath; // Объявляем переменную для пути к итоговому файлу
 
-      // Отправляем файл на фронтенд
-      const file = fs.createReadStream(outputPath);
+  try {
+    // Используем ? для безопасного вставления значений в SQL-запрос
+    const [techDataQuery] = await db.promise().query(`
+      SELECT
+        id,
+        model,
+        max_airflow_m3h,
+        max_static_pressure_pa,
+        voltage_V,
+        power_consumption_kW,
+        max_operating_current_A,
+        rotation_frequency_rpm,
+        sound_power_level_dBA,
+        airflow_temperature_range,
+        capacitor_mF,
+        electrical_connections_scheme
+      FROM zfrfans.zfr_data
+      WHERE model = ?
+    `, [selectedData.fanName]);
 
-      // Устанавливаем заголовки Content-Type и Content-Disposition
-      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-      res.setHeader('Content-Disposition', 'attachment; filename=ZFR-Datasheet.xlsx');
+    const [dimensionsQuery] = await db.promise().query(`
+      SELECT
+        id,
+        model,
+        l,
+        l1,
+        l2,
+        h,
+        d,
+        l3,
+        kg
+      FROM zfrfans.zfr_dimensions
+      WHERE model = ?
+    `, [selectedData.fanName]);
 
-      // Передача файла в ответ
-      file.pipe(res);
+    // Проверяем, есть ли данные в ответе
+    if (techDataQuery.length === 0 || dimensionsQuery.length === 0) {
+      throw new NotFoundError({ message: 'Не удалось найти данные в базе данных' });
+    }
 
-      // Удаление файла после отправки
-      file.on('close', () => {
-        fs.unlink(outputPath, (unlinkErr) => {
-          if (unlinkErr) {
-            console.error('Error deleting file:', unlinkErr);
-          } else {
-            console.log('File deleted successfully');
-          }
-        });
-      });
-    })
-    .catch((error) => {
-      console.error(error.message);
-      res.status(500).send('Internal Server Error');
+    // Извлекаем данные из ответов на SQL-запросы
+    const mySqlTechData = techDataQuery[0];
+    const mySqlDimensionsData = dimensionsQuery[0];
+
+    // Загружаем шаблон Excel-файла
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(templatePath);
+
+    // Получаем лист Excel
+    const worksheet = workbook.getWorksheet('Техника');
+
+    // Заполняем данные с фронтенда в ячейки
+    worksheet.getCell('E11').value = selectedData.systemNameValue;
+    worksheet.getCell('E10').value = selectedData.staticPressureValue;
+    worksheet.getCell('E9').value = selectedData.flowRateValue;
+
+    // Заполняем данные из таблицы zfr_data
+    worksheet.getCell('E12').value = mySqlTechData.model;
+    worksheet.getCell('F19').value = mySqlTechData.voltage_V;
+    worksheet.getCell('F20').value = mySqlTechData.power_consumption_kW;
+    worksheet.getCell('F21').value = mySqlTechData.max_operating_current_A;
+    worksheet.getCell('F22').value = mySqlTechData.rotation_frequency_rpm;
+    worksheet.getCell('F23').value = mySqlTechData.sound_power_level_dBA;
+
+    // Заполняем данные из таблицы zfr_dimensions
+    worksheet.getCell('F24').value = mySqlDimensionsData.kg;
+    worksheet.getCell('B57').value = mySqlDimensionsData.l;
+    worksheet.getCell('C57').value = mySqlDimensionsData.l1;
+    worksheet.getCell('D57').value = mySqlDimensionsData.l2;
+    worksheet.getCell('E57').value = mySqlDimensionsData.h;
+    worksheet.getCell('F57').value = mySqlDimensionsData.d;
+    worksheet.getCell('J10').value = mySqlDimensionsData.h;
+    worksheet.getCell('J9').value = mySqlDimensionsData.l1;
+    worksheet.getCell('J11').value = mySqlDimensionsData.l1;
+    worksheet.getCell('J9').value = mySqlDimensionsData.l1;
+    worksheet.getCell('G57').value = mySqlDimensionsData.l3;
+
+    // Заполняем остальные поля
+    const currentDate = new Date();
+    worksheet.getCell('J5').value = currentDate;
+
+    // Вставляем изображение в ячейку B28
+    const imageId = workbook.addImage({
+      buffer: imageBuffer,
+      extension: 'png',
     });
+
+    worksheet.addImage(imageId, {
+      tl: { col: 1, row: 28 },
+      br: { col: 8, row: 43 },
+      editAs: 'oneCell',
+    });
+
+    // Сохраняем результат в новый файл Excel
+    outputPath = path.join(__dirname, '../uploads/newDataSheet.xlsx');
+    await workbook.xlsx.writeFile(outputPath);
+
+    // Читаем содержимое файла в бинарном формате
+    const fileContent = await fs.readFile(outputPath, 'binary');
+
+    // Устанавливаем заголовки Content-Type и Content-Disposition
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.setHeader('Content-Disposition', 'attachment; filename=ZFR-Datasheet.xlsx');
+
+    // Передаем содержимое файла в ответ
+    res.write(fileContent, 'binary');
+    res.end();
+  } catch (error) {
+    console.error('Ошибка выполнения SQL-запроса:', error);
+    res.status(500).send('Внутренняя ошибка сервера');
+  } finally {
+    // Перемещаем код удаления файла за пределы блока catch
+    try {
+      if (outputPath) {
+        await fs.unlink(outputPath);
+        console.log('Файл успешно удален');
+      }
+    } catch (unlinkError) {
+      console.error('Ошибка удаления файла:', unlinkError);
+    }
+  }
 };
