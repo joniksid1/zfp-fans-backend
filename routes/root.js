7df604a482d3eeba88e21cc85d8955d346bf0dc2
Mikhail Kuznetsov <jonik.sid1@gmail.com>
2024-01-29 23:05:14 +0300

    feat: add xlsx to pdf route, controller  with local api query

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const { getDataSheet } = require('../controllers/datasheet');
 const { getFanModels, getFanDataPoints } = require('../controllers/fan-data');
+const { getPdfFromXlsx } = require('../controllers/pdf-test')
 
 router.use('/fans', getFanModels);
 router.use('/fanDataPoints', getFanDataPoints);
-router.use('/pdf', getDataSheet);
+router.use('/excel', getDataSheet);
+router.use('/excelToPdf', getPdfFromXlsx);
 
 module.exports = { router };
